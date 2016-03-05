9d5ada329967522e26c47f4dbcde2f8231f67e53
U10416020 <wkps19961015@gmail.com>
2016-03-05 14:55:31 +0800

    TestMultiplicationQuiz
    Add annotations.

--- TestMultiplicationQuiz.java
+++ TestMultiplicationQuiz.java
@@ -3,15 +3,22 @@
 import java.util.*;
 
 public class TestMultiplicationQuiz {
-	public static void main(String[] args) {
+	//Main method
+	public static void main(String[] args) {		
 		Scanner input = new Scanner(System.in);
+		
+		//Creat a object of MultiplicationQuiz
 		MultiplicationQuiz quiz = new MultiplicationQuiz();
+		
+		//While loop to ask the quiz until the answer correct
 		while(quiz.getCont() == false){
+			//Ask the quiz and check the answer
 			System.out.println("What is " + quiz.getNum1() + "*" + quiz.getNum2() + "?");
 			int ans = input.nextInt();
 			quiz.check(ans);
-			System.out.print(quiz.getResult());	
-		}
 			
+			//Display the result
+			System.out.print(quiz.getResult());	
+		}			
 	}
 }
