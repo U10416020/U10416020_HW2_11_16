9c5f93e68fd97944bafbe33f8ab96d1fa3e77a39
U10416020 <wkps19961015@gmail.com>
2016-03-05 14:58:24 +0800

    Add annotations

--- MultiplicationQuiz.java
+++ MultiplicationQuiz.java
@@ -1,20 +1,25 @@
 //U10416020
+
 import java.security.SecureRandom;
 import java.util.*;
 
 public class MultiplicationQuiz {
+	//Data field
 	ArrayList<Integer> answerList = new ArrayList<>();
-	SecureRandom random = new SecureRandom();
 	
+	SecureRandom random = new SecureRandom();	
 	int num1 = random.nextInt(10);
 	int num2 = random.nextInt(10);
 	int answer = num1 * num2;
+	
 	String result;
 	boolean cont = false;
 	
+	//Non-argument constructor
 	MultiplicationQuiz(){		
 	}
 	
+	//Method to check the answer
 	public void check(int ans){
 		if(ans == answer){
 			result = "You got it!";
@@ -32,18 +37,22 @@ public class MultiplicationQuiz {
 		answerList.add(ans);
 	}
 	
+	//Method to return result
 	public String getResult(){		
 		return result;
 	}
 	
+	//Method to return cont
 	public boolean getCont(){
 		return cont;
 	}
 	
+	//Method to return num1
 	public int getNum1(){
 		return num1;
 	}
 	
+	//Method to return num2
 	public int getNum2(){
 		return num2;
 	}
